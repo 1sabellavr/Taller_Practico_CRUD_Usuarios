3499b1c83545c1407272e003fd696f04b6d1d95e
Kevin4-b <kevego19@gmail.com>
2026-09-11 08:27:31 -0500

    Paso 11
    Se registraron las dependencias en Program.cs

--- UsuarioApi/Program.cs
+++ UsuarioApi/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using UsuarioApi.Data;
+using UsuarioApi.Interfaces;
+using UsuarioApi.Repositories;
+using UsuarioApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,23 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // 1. Registrar soporte para Controladores
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // 2. Registrar AppDbContext conectándolo a SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configurar OpenAPI / Swagger
-builder.Services.AddOpenApi();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 var app = builder.Build();
 
 // Coonfigurar el pipeline de solicitudes HTTP.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
 app.Run();
